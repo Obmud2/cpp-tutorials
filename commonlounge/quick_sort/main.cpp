f2a15b8779b49e320601ead4912b5b72715df522
Obmud2 <50120231+Obmud2@users.noreply.github.com>
2021-01-17 14:38:06 +0000

    Modify quick sort to in-place algo

--- commonlounge/quick_sort/main.cpp
+++ commonlounge/quick_sort/main.cpp
@@ -9,49 +9,63 @@
 #include <fstream>
 using namespace std;
 
-int qs_partition(int arr[], int begin, int end){
-    int temp_arr[end - begin];
-    int temp_i{0};
-    int pivot {rand() % (end - begin) + begin};
-    //cout << "Pivot: " << pivot << " Begin: " << begin << " End: " << end << endl;
-    
-    for (int i{begin}; i<end; i++){
-        if (arr[i] <= arr[pivot] && i != pivot){
-            temp_arr[temp_i] = arr[i];
-            temp_i++;
-        }
-    }
-    temp_arr[temp_i] = arr[pivot];
-    int pivot_result {begin + temp_i};
-    temp_i++;
-    for (int i{begin}; i<end; i++){
-        if (arr[i] > arr[pivot]){
-            temp_arr[temp_i] = arr[i];
-            temp_i++;
-        }
-    }
-    for (int i{begin}; i<end; i++){
-        arr[i] = temp_arr[i-begin];
-    }
-    
-    return pivot_result;
+void swap(int &a, int &b){
+    int temp{a};
+    a = b;
+    b = temp;
 }
 
-void quick_sort(int arr[], int begin, int end){
+int qs_partition(int arr[], int begin, int end){
+
+    // Select pivot
+    int pivot_i {rand() % (end - begin) + begin};
+    int pivot {arr[pivot_i]};
+    //cout << "B:" << begin << " E:" << end << " Pv:" << pivot << endl;
+    
+    // Initiate selectors
+    int low{begin}, high{end-1};
+    int last_low{-1}, last_high{end};
+    
+    while (low < high){
+        // Move to next pair to swap
+        while (arr[low]  < pivot && low  <  end  ) {low++;}
+        while (arr[high] > pivot && high >= begin) {high--;}
+        
+        // Check the swap is not repeated in the case of duplicate pivots
+        if (low == last_low && high == last_high){
+            low++;
+            continue;
+        }
+        
+        // Swap elements
+        swap(arr[low], arr[high]);
+        
+        // Store last swap
+        last_low = low;
+        last_high = high;
+    }
+    
+    // Return pivot index
+    return low;
+}
+
+void quick_sort(int arr[], int begin, int end, long &count){
     if ((end - begin) <= 1){
         return;
     }
     
     int pivot{qs_partition(arr, begin, end)};
+    count++;
     
-    quick_sort(arr, begin, pivot);
-    quick_sort(arr, pivot+1, end);
+    quick_sort(arr, begin, pivot, count);
+    quick_sort(arr, pivot+1, end, count);
     
 }
 
 int main(int argc, const char * argv[]) {
     
     srand(97);
+    long count{0};
     
     ifstream ifs;
     ofstream ofs;
@@ -76,12 +90,12 @@ int main(int argc, const char * argv[]) {
         }
         cout << endl;
         
-        int *array { new int[N]{} };
+        int *array { new int[N]{} }; // Dynamic array for file read
         
         for (int n{0}; n<N; n++){
             ifs >> array[n];            // Read N elements
         }
-        quick_sort(array, 0, N);   // Sort array
+        quick_sort(array, 0, N, count);   // Sort array
         
         for (int n{0}; n<N; n++){
             ofs << array[n] << " ";     // Write array to output
@@ -89,6 +103,9 @@ int main(int argc, const char * argv[]) {
         delete [] array;
         ofs << endl;
     }
+    
+    ifs.close();
+    ofs.close();
     
     return 0;
 }
